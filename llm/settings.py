096b7788e45ecabbedc9b91575f9a2de5d315bb9
Him10Agr <himanshuagr.c2@gmail.com>
2025-03-09 00:10:54 +0530

    Created the inrastructure .env file, pydantic settings and connections for mongodb and qdrant databases

---
+++ llm/settings.py
@@ -0,0 +1,96 @@
+from loguru import logger
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from zenml.client import Client
+from zenml.exceptions import EntityExistsError
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    # OpenAI API
+    OPENAI_MODEL_ID: str = "gpt-4o-mini"
+    OPENAI_API_KEY: str | None = None
+
+    # Huggingface API
+    HUGGINGFACE_ACCESS_TOKEN: str | None = None
+
+    # Comet ML (during training)
+    COMET_API_KEY: str | None = None
+    COMET_PROJECT: str = "twin"
+
+    # --- Required settings when deploying the code. ---
+    # --- Otherwise, default values values work fine. ---
+
+    # MongoDB database
+    DATABASE_HOST: str = "mongodb://rnr-system-chatbot:lrnr-system-chatbot@127.0.0.1:27017"
+    DATABASE_NAME: str = "rnr-system-chatbot"
+
+    # Qdrant vector database
+    USE_QDRANT_CLOUD: bool = False
+    QDRANT_DATABASE_HOST: str = "localhost"
+    QDRANT_DATABASE_PORT: int = 6333
+    QDRANT_CLOUD_URL: str = "str"
+    QDRANT_APIKEY: str | None = None
+
+
+    # RAG
+    TEXT_EMBEDDING_MODEL_ID: str = "sentence-transformers/all-MiniLM-L6-v2"
+    RERANKING_CROSS_ENCODER_MODEL_ID: str = "cross-encoder/ms-marco-MiniLM-L-4-v2"
+    RAG_MODEL_DEVICE: str = "cpu"
+
+
+    @property
+    def OPENAI_MAX_TOKEN_WINDOW(self) -> int:
+        official_max_token_window = {
+            "gpt-3.5-turbo": 16385,
+            "gpt-4-turbo": 128000,
+            "gpt-4o": 128000,
+            "gpt-4o-mini": 128000,
+        }.get(self.OPENAI_MODEL_ID, 128000)
+
+        max_token_window = int(official_max_token_window * 0.90)
+
+        return max_token_window
+
+    @classmethod
+    def load_settings(cls) -> "Settings":
+        """
+        Tries to load the settings from the ZenML secret store. If the secret does not exist, it initializes the settings from the .env file and default values.
+
+        Returns:
+            Settings: The initialized settings object.
+        """
+
+        try:
+            logger.info("Loading settings from the ZenML secret store.")
+
+            settings_secrets = Client().get_secret("settings")
+            settings = Settings(**settings_secrets.secret_values)
+        except (RuntimeError, KeyError):
+            logger.warning(
+                "Failed to load settings from the ZenML secret store. Defaulting to loading the settings from the '.env' file."
+            )
+            settings = Settings()
+
+        return settings
+
+    def export(self) -> None:
+        """
+        Exports the settings to the ZenML secret store.
+        """
+
+        env_vars = settings.model_dump()
+        for key, value in env_vars.items():
+            env_vars[key] = str(value)
+
+        client = Client()
+
+        try:
+            client.create_secret(name="settings", values=env_vars)
+        except EntityExistsError:
+            logger.warning(
+                "Secret 'scope' already exists. Delete it manually by running 'zenml secret delete settings', before trying to recreate it."
+            )
+
+
+settings = Settings.load_settings()
